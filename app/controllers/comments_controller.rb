1705d4c4a609a445c52a613b43e74f7cf4d19b13
bytheway875 <s.byrne2254@gmail.com>
2013-07-29 15:47:09 -0500

    Added some styling for show view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   before_filter :authenticate_user!
 
   def create
-    @comment = current_user.comments.create(params[:comment].merge(:tattoo_id: params[:tattoo_id]))
+    @comment = current_user.comments.create(params[:comment].merge(:tattoo_id => params[:tattoo_id]))
     redirect_to :back
   end
 
