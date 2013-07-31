c965328b7e42cc49ed1e2b7c25ff6cb5f44c5d4e
bytheway875 <s.byrne2254@gmail.com>
2013-07-31 14:17:49 -0500

    created helper methods to use devise inside of other views

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :role_ids, :as => :admin
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :location
   
   # Establish relationships between models
   has_many :tattoos
