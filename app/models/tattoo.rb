aeea34857a2136faccebfe0452dde470669b8362
tinasoul <tinapshah@gmail.com>
2013-07-31 17:14:24 -0500

    Added search bar to nav

--- app/models/tattoo.rb
+++ app/models/tattoo.rb
@@ -3,5 +3,9 @@ class Tattoo < ActiveRecord::Base
   attr_accessible :picture, :description, :gender, :body_location, :studio, :artist, :hashtags, :location
   mount_uploader :picture, PictureUploader
   has_many :comments
-  
+
+  # searchable do 
+  #   text :gender, :body_location, :studio, :artist, :hashtags, :location
+  # end
+
 end
