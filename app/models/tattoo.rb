72f4b5d917b46aa689b7235cfed621cdb84f3e87
tinasoul <tinapshah@gmail.com>
2013-08-01 17:09:07 -0500

    Commented solr search stuff before heroku push.

--- app/models/tattoo.rb
+++ app/models/tattoo.rb
@@ -4,19 +4,19 @@ class Tattoo < ActiveRecord::Base
   mount_uploader :picture, PictureUploader
   has_many :comments
 
-  searchable do 
-    text :gender, :body_location, :studio, :artist, :hashtags, :location
-  end
+  # searchable do 
+  #   text :gender, :body_location, :studio, :artist, :hashtags, :location
+  # end
 
-  def self.search(query, params={})
-    solr_search do
+  # def self.search(query, params={})
+  #   solr_search do
 
-      fulltext query do
-        minimum_match 1
-      end
+  #     fulltext query do
+  #       minimum_match 1
+  #     end
 
-      paginate :page => params[:page], :per_page => params[:per_page]
-    end
-  end
+  #     paginate :page => params[:page], :per_page => params[:per_page]
+  #   end
+  # end
 
 end
