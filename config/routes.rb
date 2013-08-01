8646e115ef5ec9efd536a6bda7f5c10d24571353
tinasoul <tinapshah@gmail.com>
2013-08-01 15:06:58 -0500

    Changed route so login and signup redirects to tattoos index.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Myapp::Application.routes.draw do
 
 
   authenticated :user do
-    root :to => 'home#index'
+    root :to => 'tattoos#index'
   end
   root :to => "home#index"
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}, controllers: {omniauth_callbacks: "omniauth_callbacks"}
