3bf4eb9efe07dd0564356856206150414354b28b
bytheway875 <s.byrne2254@gmail.com>
2013-08-02 15:44:47 -0500

    Changed styling on tattoo show page, experimenting with Foundation buttons

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
   attr_accessible :message, :tattoo_id, :comment_content
+  validates :comment_content, :presence => true
 
   belongs_to :user
   belongs_to :tattoo
