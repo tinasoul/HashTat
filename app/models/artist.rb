6d1fb7aea1c6ede53123e0f92d99e021e7e9f720
Shannon <s.byrne2254@gmail.com>
2013-08-09 00:19:05 -0500

    created methods on Artist model to create links for social media

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,15 +1,46 @@
 class Artist < ActiveRecord::Base
   belongs_to :user
   attr_accessible :last_name, :first_name, :full_name, :location, :tattoo_shop, :avatar, :specialties, :bio, :instagram, :flickr, :twitter, :facebook, :cover_tattoo, :user_id
+  require "uri"
+
+  #Validations
   validates_presence_of :first_name
+  validates :twitter, :format => { without: /\W/, allow_blank: true, :message => 'Enter a vaild twitter handle without @.'}
+  validates :flickr, :format => { without: /\W/, allow_blank: true, :message => 'Enter a valid flickr username or a link to your flickr profile.'}
+  validates :instagram, :format => { without: /\W/, allow_blank: true, :message => 'Enter a valid instagram username or a link to your instagram profile.'}
+  validates :facebook, :format => { without: /\W/, allow_blank: true, :message => 'Enter a valid facebook url.'}
 
 
+  # Creates a "full name" for the artist, which is a combination of first and last name
+  # If only first name exists, full name is set as first name
+  # Will be used for searching through artists and potentially for artist route
   def full_name
     if self.first_name && self.last_name
       full_name = "#{self.first_name} #{self.last_name}"
       self.full_name = full_name
     else
-        self.full_name = self.first_name
+      self.full_name = self.first_name
     end
   end
+
+
+  def add_http(input)
+    u=URI.parse(input)
+    if (!u.scheme)
+        return link = "http://" + input
+    else
+        return link = input
+    end
+  end
+
+  def build_link(social_network)
+    if self[social_network]
+      if self[social_network].include?('/')
+        self.add_http(self[social_network])
+      else
+        "http://" + social_network + ".com/#{self[social_network]}"
+      end
+    end
+  end
+
 end
--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -2,15 +2,16 @@ require 'spec_helper'
 
 describe Artist do
   describe 'validations' do
-    context 'when a first name is not entered'
-      it 'should not save the artist'
+    context 'when a first name is not entered' do
+      it 'should not save the artist' do
           Artist.new(:first_name => '').should_not be_valid
-        end
+      end
     end
+  end
 
   describe '#full_name' do
   let(:artist){ Artist.new }
-    context 'when a frist name and last name are entered' do
+    context 'when a first name and last name are entered' do
         it "should set the artist's full name equal to their first and last name combined" do
           artist.first_name = "Matt"
           artist.last_name = "Buck"
@@ -24,6 +25,44 @@ describe Artist do
           expect(artist.full_name).to eq("Matt")
         end
     end
-    
+  end
+
+  describe '#add_http' do
+  let(:artist){ Artist.new(:first_name => 'Shannon') }
+    context 'when the method is sent a url' do
+      it "append http:// if it doesnt have it already" do
+        expect(artist.add_http('www.facebook.com')).to eq('http://www.facebook.com')
+      end
+    end
+  end
+
+  describe '#facebook_link' do
+    context 'when a user enters something in to the :facebook field' do
+      it "should check to see whether the user entered a url or just a page name"
+      it "should append http:// if it is a url and it doesn't have it already"
+      it "should turn the page name in to a link"
+    end
+  end
+
+  describe '#twitter_link' do
+    context 'when a user enters something in to the :twitter field' do
+      it "should turn the twitter handle in to a twitter link"
+    end
+  end
+
+  describe '#instagram_link' do
+    context 'when a user enters something in to the :instagram field' do
+      it "should check to see whether the user entered a url or just a username"
+      it "should append http:// to urls that don't have http"
+      it "should create a link from the username"
+    end
+  end
+  
+  describe '#flickr_link' do
+    context 'when a user enters something in to the :flickr field' do
+      it "should check to see whether the user entered a url or just a flickr username"
+      it "should append http:// to urls that don't have http://"
+      it "should create a full link from the username"
+    end
   end
 end
