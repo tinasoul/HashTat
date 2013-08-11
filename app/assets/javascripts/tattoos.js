f8b1938e997c5240d6f49c8084be0fb26ec488a9
Shannon <s.byrne2254@gmail.com>
2013-08-10 22:54:13 -0500

    Added jquery to auto resize modal size depending on window size

--- app/assets/javascripts/tattoos.js
+++ app/assets/javascripts/tattoos.js
@@ -2,4 +2,20 @@ $(function(){
 	$('.closeModal').click(function(){
     $('#myModal').foundation('reveal', 'close');
   });
+
+
+$(window).resize(function(){
+   console.log('resize called');
+   var width = $(window).width();
+   if(width >= 960 && width <= 1200){
+       $('#myModal').removeClass('large').addClass('medium');
+   }
+   else if (width < 960 && width > 500){
+       $('#myModal').removeClass('small').addClass('large');
+   }
+   else {
+   	$('#myModal').removeClass('medium').addClass('small');
+   }
+})
+$(document).resize();
 });
