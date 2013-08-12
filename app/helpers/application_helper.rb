1e52d5c76c0899ecddc265855f12e56428ca4856
tinasoul <tinapshah@gmail.com>
2013-08-12 15:15:03 -0500

    Added twitterfeed in nav bar to show diff in an index pg.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,4 +23,12 @@ module ApplicationHelper
       end
     end
   end
+
+  def show_tattoo_photo(tattoo)
+    if tattoo.twitter_photo.blank?
+      link_to image_tag(tattoo.picture_url(:medium)), tattoo 
+    else
+      link_to image_tag(tattoo.twitter_photo), tattoo 
+    end
+  end
 end
--- app/helpers/tattoos_helper.rb
+++ app/helpers/tattoos_helper.rb
@@ -1,9 +1,9 @@
 module TattoosHelper
-  def tweeted_a_photo(tattoo)
-    if tattoo.twitter_photo.blank?
-      link_to image_tag(tattoo.picture_url(:medium)), tattoo 
-    else
-      link_to image_tag(tattoo.twitter_photo), tattoo 
-    end
-  end
+  # def show_tattoo_photo(tattoo)
+  #   if tattoo.twitter_photo.blank?
+  #     link_to image_tag(tattoo.picture_url(:medium)), tattoo 
+  #   else
+  #     link_to image_tag(tattoo.twitter_photo), tattoo 
+  #   end
+  # end
 end
