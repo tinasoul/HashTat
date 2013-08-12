7c8a1d4b43000d6de4739ff203d8026bcc9147cd
tinasoul <tinapshah@gmail.com>
2013-08-12 18:39:45 -0500

    Valid uniqueness from Tweet model.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,7 +1,7 @@
 class Tweet < ActiveRecord::Base
   attr_accessible :text, :pulled_tweet_id, :attached_photos, :handle, :hashtags, :twitter_photo
   
-  has_many :tattoos
+  validates_uniqueness_of :pulled_tweet_id
   serialize :attached_photos
 
   def self.pull_tweets
@@ -38,32 +38,7 @@ class Tweet < ActiveRecord::Base
 end
 
 
-#  def self.pull_tweets
-#     Twitter.search("#hashtat").results.map do |tweet|
-#      create!(pulled_tweet_id: tweet.id.to_s,
-#       text: tweet.text,
-#       attached_photos: tweet.media,
-#       handle: tweet.user.screen_name)
-#       # pulled_twitter_hashtag: tweet.text.hashtag
 
-#       if User.find_by_username(tweet.user.screen_name)
-#         u = User.find_by_username(tweet.user.screen_name)
-
-#         tweet.media.each do |photo|
-#           uploader = PictureUploader.new
-#           File.write("public/tattoos/#{photo.id}.jpg", open(photo.media_url).read, {mode: 'wb'})
-#           uploader.store!(File.open("public/tattoos/#{photo.id}.jpg"))
-#           @tattoo = u.tattoos.create(description: tweet.text, picture: photo)
-#        else
-#         tweet.media.each do |photo|
-#           uploader = PictureUploader.new
-#           File.write("public/tattoos/#{photo.id}.jpg", open(photo.media_url).read, {mode: 'wb'})
-#           uploader.store!(File.open("public/tattoos/#{photo.id}.jpg"))
-#         end
-#       end
-#     end
-#   end
-# end
 
 
 
