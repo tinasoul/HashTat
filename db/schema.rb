8b48a4e32dd69d482e71be730bf3a980041a0eb0
tinasoul <tinapshah@gmail.com>
2013-08-13 14:33:57 -0500

    Merged artist portfilio steps.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130808190734) do
+ActiveRecord::Schema.define(:version => 20130809213909) do
 
   create_table "artists", :force => true do |t|
     t.string   "first_name"
