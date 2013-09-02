5d7eb8691d66a93983e7b6f00580a401d89bfe65
Shannon Byrne <s.byrne2254@gmail.com>
2013-09-01 21:57:27 -0500

    added ability for a user to delete a review they've made

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,10 @@
 class CommentsController < ApplicationController
   before_filter :authenticate_user!
 
+  def show
+    @comments = Comment.all
+  end
+
   def create
     @comment = if params[:artist_id]
       current_user.comments.create(params[:comment].merge(:artist_id => params[:artist_id]))
@@ -11,12 +15,18 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-  	@tattoo = Tattoo.find(params[:tattoo_id])
-  	@comment = @tattoo.comments.find(params[:id])
-    @comment.destroy
+    if params[:tattoo_id]
+      @tattoo = Tattoo.find(params[:tattoo_id])
+  	  @comment = @tattoo.comments.find(params[:id])
+      @comment.destroy
+    elsif params[:artist_id]
+      @artist = Artist.find(params[:artist_id])
+      @comment = @artist.comments.find(params[:id])
+      @comment.destroy
+    end
 
     respond_to do |format|
-      format.html { redirect_to tattoo_path(@tattoo) }
+      format.html { redirect_to :back }
       format.json { head :no_content }
     end
   end
