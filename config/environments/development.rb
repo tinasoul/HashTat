3eabfc00819cef1466a6167261782cb1bdae988f
mifan <mifanxp@gmail.com>
2013-10-19 20:36:51 +0800

    delivery mail on development env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,6 +42,8 @@ Myapp::Application.configure do
   config.assets.debug = true
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
