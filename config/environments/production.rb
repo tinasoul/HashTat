33df6ec4ba6eba348865aa1b069e917d636f736f
Micah Frazier <micah.frazier82@gmail.com>
2013-10-21 13:35:25 -0500

    change password and confirm emails working

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,11 +73,11 @@ Myapp::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: ENV["DOMAIN_NAME"],
+    domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: "mrhashtat@gmail.com",
-    password: "a1b2c3d45"
+    password: "workhardplayhard"
   }
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
