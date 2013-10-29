2c59f7ccc06c42f729722dfca3f1ddaff687308b
Blane Cordes <blane.cordes@echostar.com>
2013-10-29 11:18:39 -0400

    update with new email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < Devise::Mailer
-  default from: "mrhashtat@gmail.com"
+  default from: "mshashtat@gmail.com"
 
   def welcome_email(user)
     @user = user
     @url  = "https://hashtat.com"
-    mail(:to => user.email, :subject => "Welcome to HashTat!", :from => "mrhashtat@gmail.com")
+    mail(:to => user.email, :subject => "Welcome to HashTat!", :from => "mshashtat@gmail.com")
   end
 
 end
--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,8 +1,8 @@
 ActionMailer::Base.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
-    :user_name => "mrhashtat@gmail.com",
-    :password => "workhardplayhard",
+    :user_name => "mshashtat@gmail.com",
+    :password => "dontletgo3",
     :authentication => "plain",
     :enable_starttls_auto => true
   }
