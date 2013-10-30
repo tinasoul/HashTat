5c20476152a7842d0dee90b6fc00c01c7c5bbd1d
Sonny Gonzalez <sonnyg.email@gmail.com>
2013-10-30 13:23:48 -0500

    install machinist and add User blueprint

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+require 'faker'
+require Rails.root.join('spec', 'support', 'blueprints')
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,12 +8,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-puts 'ROLES'
-YAML.load(ENV['ROLES']).each do |role|
-  Role.find_or_create_by_name({ :name => role }, :without_protection => true)
-  puts 'role: ' << role
-end
-puts 'DEFAULT USERS'
-user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
-puts 'user: ' << user.username
-user.add_role :admin
+
+# puts 'ROLES'
+# YAML.load(ENV['ROLES']).each do |role|
+#   Role.find_or_create_by_name({ :name => role }, :without_protection => true)
+#   puts 'role: ' << role
+# end
+# puts 'DEFAULT USERS'
+# user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
+# puts 'user: ' << user.username
+# user.add_role :admin
---
+++ spec/support/blueprints.rb
@@ -0,0 +1,14 @@
+require 'faker'
+require 'machinist/active_record'
+
+User.blueprint do
+  about_me { Faker::Lorem.paragraph(5) }
+  email { Faker::Internet.email }
+  fav_artist { Faker::Name.name }
+  password { "test1234" }
+  password_confirmation { "test1234" }
+  location { 'Austin, TX' }
+  memorable_story { Faker::Lorem.paragraph(10) }
+  tat_number { 1 + rand(100).to_i }
+  username { Faker::Name.first_name }
+end
