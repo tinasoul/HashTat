ff4328cb32ebd68be8c9f548f74ec9660b19a4a0
Blane Cordes <blane.cordes@echostar.com>
2013-10-30 23:41:42 -0400

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Myapp::Application.configure do
   }
 
 
-  config.action_mailer.default_url_options = { :host => 'example.com' }
+  config.action_mailer.default_url_options = { :host => 'https://hashtat.herokuapp.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
