3f34ba77db45653e012cb38176dfd6dbf4c34bb7
Blane Cordes <blane.cordes@echostar.com>
2013-10-30 23:29:14 -0400

    fix

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -13,4 +13,12 @@ class UserMailer < ActionMailer::Base
     devise_mail(record, :reset_password_instructions, opts)
   end
 
+  def update_with_password(params, *options)
+    if encrypted_password.blank?
+      update_attributes(params, *options)
+    else
+      super
+    end
+  end
+
 end
