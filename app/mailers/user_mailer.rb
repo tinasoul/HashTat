a76a6fbb97e50c738af577cc9c9e36d1e154a944
Blane Cordes <blane.cordes@echostar.com>
2013-10-30 23:38:17 -0400

    fix

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,15 @@
-class UserMailer < ActionMailer::Base
-  include Devise::Mailers::Helpers
+class UserMailer < Devise::Mailer
   default from: "mshashtat@gmail.com"
 
   def welcome_email(user)
     @user = user
     @url  = "https://hashtat.com"
     mail(:to => user.email, :subject => "Welcome to HashTat!", :from => "mshashtat@gmail.com")
+  end
+
+  def reset_password_instructions(record, token, opts={})
+    @token = token
+    devise_mail(record, :reset_password_instructions, opts)
   end
 
 end
