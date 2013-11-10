71a161478c85ae9d1d32d49bbbeed7bf5934d782
Blane Cordes <blane.cordes@echostar.com>
2013-11-10 12:03:12 -0500

    comment fix

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,8 @@
 class Comment < ActiveRecord::Base
   attr_accessible :message, :tattoo_id, :comment_content, :artist_id
+  attr_accessible :id, :user_id, :tattoo_id, :comment_content, :created_at, 
+                  :updated_at, :artist_id, :as => [:default, :admin]
+
   validates :comment_content, :presence => true
 
   belongs_to :user
